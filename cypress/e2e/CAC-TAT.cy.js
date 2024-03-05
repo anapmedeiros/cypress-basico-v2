c6bc6d86eb0a87413cb39026a25f8a697c3a6f97
anapmedeiros <anapmedeiros@gmail.com>
2024-03-05 12:54:13 -0300

    Create cypress project

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -174,7 +174,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
 
     it('seleciona um arquivo da pasta fixtures', function () {
-        cy.get('input[type="file"]').selectFile('C:/Users/Usuario/Desktop/Projetos/CyBasic/cypress-basico-v2/cypress/fixtures/example.json')
+        cy.get('input[type="file"]').selectFile('./cypress/fixtures/example.json')
             .should(function ($input) {
                 expect($input[0].files[0].name).to.equal('example.json')
                 })
